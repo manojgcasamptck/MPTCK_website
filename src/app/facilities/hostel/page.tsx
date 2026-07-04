63849965a6b1bef600a200820dfef24ce06e9099
Manoj G <manojg@Manojs-MacBook-Air.local>
2026-07-04 22:59:59 +0530

    Update hostel

--- src/app/facilities/hostel/page.tsx
+++
@@ -1,6 +0,0 @@
-//import ContentPage from "@/components/ContentPage";
-//import { pageContents } from "@/lib/data";
-
-//export default function Page() {
-  //return <ContentPage content={pageContents["/facilities/hostel"]} />;
-//}
